119512665a1ee5edcdb9d2250e69871eaeb99f44
Satwik <109500739+Satwik-uppada782@users.noreply.github.com>
2023-04-21 00:25:36 +0530

    Tkinter Project

--- projects_in_python/email validation using functions.py
+++ projects_in_python/email validation using functions.py
@@ -1,25 +1,118 @@
-email=input("enter the email:")
+email = input("Enter the Email address: ")
+
+
 def validation(email):
-    if ("@" in email):
-        domain_split=email.split("@")
-        domain_part_validation(domain_split)
-        domain_validation()
-
+    if (" " in email) == False:
+            if not email.islower():
+                return False
+            else:
+                if ("@" in email) and (email.count("@") == 1):
+                    email_split = email.split("@")
+                    if len(email_split) == 2:
+                        username, domain = email_split
+                        if username.isalnum() == True:
+                            if username[0].isalpha() == True:
+                                if username[-1].isalnum() == True:
+                                    dot_commercial = domain.split(".")
+                                    if len(dot_commercial) == 2:
+                                        email_service, domain_name = dot_commercial
+                                        if len(domain_name) >= 2:
+                                            if email_service.isdigit() == False:
+                                                if email_service.isalpha() == True:
+                                                    return True
+                                                else:
+                                                    return False
+                                            else:
+                                                return False
+                                        else:
+                                            return False
+                                    else:
+                                        return False
+                                else:
+                                    return False
+                            else:
+                                return False
+                        elif (("." in username) or ("_" in username) or ("-" in username)):
+                            if username[-1].isalnum() and username[0].isalnum() == True:
+                                if "." in username:
+                                    first_name, last_name = username.split(".", 1)
+                                    if first_name[-1].isalnum() and last_name[0].isalnum() == True:
+                                        dot_commercial = domain.split(".")
+                                        if len(dot_commercial) == 2:
+                                            email_service, domain_name = dot_commercial
+                                            if len(domain_name) >= 2:
+                                                if email_service.isdigit() == False:
+                                                    if email_service.isalpha() == True:
+                                                        return True
+                                                    else:
+                                                        return False
+                                                else:
+                                                    return False
+                                            else:
+                                                return False
+                                        else:
+                                            return False
+                                    elif (first_name[-1] or last_name[0] == (".") or ("_") or ("-")):
+                                        return False
+                                    else:
+                                        return False
+                                elif ("_" in username):
+                                    first_name, last_name = username.split("_", 1)
+                                    if first_name[-1].isalnum() and last_name[0].isalnum() == True:
+                                        dot_commercial = domain.split(".")
+                                        if len(dot_commercial) == 2:
+                                            email_service, domain_name = dot_commercial
+                                            if len(domain_name) >= 2:
+                                                if email_service.isdigit() == False:
+                                                    if email_service.isalpha() == True:
+                                                        return True
+                                                    else:
+                                                        return False
+                                                else:
+                                                    return False
+                                            else:
+                                                return False
+                                        else:
+                                            return False
+                                    elif (first_name[-1] or last_name[0] == (".") or ("_") or ("-")):
+                                        return False
+                                    else:
+                                        return False
+                                elif "-" in username:
+                                    first_name, last_name = username.split("-", 1)
+                                    if first_name[-1].isalnum() and last_name[0].isalnum() == True:
+                                        dot_commercial = domain.split(".")
+                                        if len(dot_commercial) == 2:
+                                            email_service, domain_name = dot_commercial
+                                            if len(domain_name) >= 2:
+                                                if email_service.isdigit() == False:
+                                                    if email_service.isalpha() == True:
+                                                        return True
+                                                    else:
+                                                        return False
+                                                else:
+                                                    return False
+                                            else:
+                                                return False
+                                        else:
+                                            return False
+                                    elif (first_name[-1] or last_name[0] == (".") or ("_") or ("-")):
+                                        return False
+                                    else:
+                                        return False
+                                else:
+                                    return False
+                            else:
+                                return False
+                        else:
+                            return False
+                    else:
+                        return False
+                else:
+                    return False
 
     else:
         return False
 
-def domain_part_validation(domain_split):
-     if len(domain_split)==2:
-         username,domain=domain_split
-         return domain,username
-     else:
-        return False
 
-def domain_validation(domain):
-    dot_commercial=domain.split(".")
-    if len(dot_commercial)==2:
-        email_service,domain_name=dot_commercial
-    else:
-        return False
-
+print(validation(email))
